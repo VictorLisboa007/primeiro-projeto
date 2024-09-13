db7e9ed358052b0bea6159374b7debd4c7c23281
Victor Hugo <victor.h.cerqueira@aluno.senai.br>
2024-09-13 16:34:56 -0300

    Adicionando CSS ao React

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 import FirstComponent from './components/FirstComponent.jsx';
 import TemplateExpressions from './components/TemplateExpressions.jsx';
 import MyComponent from './components/MyComponent.jsx';
 
+import vegeta from './assets/vegeta.jpg'
+import OutroComponent from './components/OutroComponent.jsx';
+import ComponentORemake from './components/ComponentORemake.jsx';
+
 
 function App() {
+  const n = 15
+  const classeDiferente = true
+  
   return (
     <div className='App'>
-        <FirstComponent/>
+        {/*Importação pela pasta public*/}
+        <h1>Goku SSj3</h1>
+        <img src="./goku.jpg" width={900} height={600}></img>
+        <br></br>
+        {/* <FirstComponent/>
         <TemplateExpressions/>
-        <MyComponent/>
+        <MyComponent/> */}
+
+        {/*Importação pela pasta assets*/}
+        <h1>Majin Vegeta</h1>
+        <img src={vegeta} alt="Imagem do Vegeta" width={900} height={600}/>
+        
+        <OutroComponent/>
+        {/*CSS inline*/}
+        <p style={{color:"blue", padding:"25px", backgroundColor:"lightblue", fontSize:"40px"}}>Este parágrafo é do App.jsx</p>
+        {/*CSS com if ternário*/}
+        <h3 style={n > 10 ? ({color:"green", backgroundColor:"yellow"}) : ({color:"gray", backgroundColor:"black"}) }>AQUI TEM UM TEXTO COM O PRIMEIRO ESTILO</h3>
+        <h3 style={n < 10 ? ({color:"green", backgroundColor:"yellow"}) : ({color:"gray", backgroundColor:"black"}) }>AQUI TEM UM TEXTO COM O PRIMEIRO ESTILO</h3>
+        
+        {/*Class diferente de acordo com a verificação*/}
+        <h2 className={classeDiferente ? "red-title" : "normal-title"}>TEXTO COM CLASSE DIFERENTE 1</h2>
+        <h2 className={classeDiferente ? "normal-title" : "red-title"}>TEXTO COM CLASSE DIFERENTE 2 - O RETORNO</h2>
+
+        <ComponentORemake/>
     </div>
   );
 }
---
+++ src/components/OutroComponent.jsx
@@ -0,0 +1,12 @@
+import './OutroComponent.css'
+
+const OutroComponent = () => {
+  return (
+    <div>
+        <h1>TEXTO DO OUTROCOMPONENT</h1>
+        <p className='douradoPreto'>PARÁGRAFO DO OUTROCOMPONENT</p>
+    </div>
+  )
+}
+
+export default OutroComponent
